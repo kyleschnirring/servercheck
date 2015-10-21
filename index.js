628d1c7dd1b3bf2a11f3461247f70b3a8fd756a9
kyleschnirring <kyleschnirring@icloud.com>
2015-10-20 17:47:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var nodemailer = require('nodemailer');
+var http = require('http');
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+
+var mailOptions = {
+    from: 'Server', // sender address
+    to: '2066128681@vtext.com', // list of receivers
+    subject: 'Websites', // Subject line
+    text: 'Websites still up', // plaintext body
+    html: '<br>Websites still up</br>' // html body
+};
+
+http.get("http://donttattleonme.com/index.html", function (res) {
+  console.log("Got response: " + res.statusCode);
+    if (res.statusCode === 200) {
+      transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            return console.log(error);
+          }
+            console.log('Message sent: ' + info.response);
+        });
+    }
+}).on('error', function (e) {
+    console.log("Got error: " + e.message);
+});
+
